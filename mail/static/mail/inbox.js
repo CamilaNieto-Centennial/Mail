d7713ecb7260eefd39b31a431b9c5f6676e9deda
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-11-24 20:17:09 -0500

    Archive and Unarchive feature Done. View button added

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -62,13 +62,94 @@ function load_mailbox(mailbox) {
 
         // Contents of the div
         newEmail.innerHTML =
-          `<h5 style="color: Navy">${email.sender}</h5>
-         <p><strong>${email.subject}</strong></p>
-         <p>${email.timestamp}</p>
-          `
+        `<h5 style="color: Navy">${email.sender}</h5>
+        <p><strong>${email.subject}</strong></p>
+        <p>${email.timestamp}</p>
+        `
 
-        // Click event to check the new email
-        newEmail.addEventListener('click', function () {
+        // View button created and added to the new email
+        let viewButton = document.createElement('button');
+        viewButton.className = "btn btn-primary";
+        viewButton.dataset.email = email.id;
+        viewButton.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
+            <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765l-6-3.2ZM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516ZM0 13.373l5.693-3.163L0 6.873v6.5Z"/>
+          </svg>
+          View
+        `;
+        viewButton.style.margin = '0em .5em 0em 0em';
+        newEmail.appendChild(viewButton)
+
+
+        // Contents of the div for Inbox
+        if (mailbox === 'inbox') {
+          // If email is NOT archived...
+          if (email.archived === false) {
+            // Create and Add Archive button to the 'div'
+            let archiveButton = document.createElement('button');
+            archiveButton.className = "btn btn-success";
+            archiveButton.dataset.email = email.id
+            archiveButton.innerHTML = `
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+              </svg>
+              Archive
+            `;
+            newEmail.appendChild(archiveButton)
+
+            //If Archive button is clicked, set 'archived' property to true
+            archiveButton.addEventListener('click', function() {
+              fetch(`/emails/${email.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                  archived: true
+                })
+              })
+              .then(() => { load_mailbox('archive') })
+            })
+
+          }
+          
+          
+        }
+
+        // Contents of the div for Archive
+        else if (mailbox === 'archive') {
+          // If email is archived...
+          if (email.archived === true) {
+            // Create and Add Unarchive button to the 'div'
+            let unarchiveButton = document.createElement('button');
+            unarchiveButton.className = "btn btn-danger";
+            unarchiveButton.dataset.email = email.id
+            unarchiveButton.innerHTML = `
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+              </svg>
+              Unarchive
+            `;
+            newEmail.appendChild(unarchiveButton)
+
+            //If Unarchive button is clicked, set 'archived' property to false
+            unarchiveButton.addEventListener('click', function() {
+              fetch(`/emails/${email.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                  archived: false
+                })
+              })
+              .then(() => { load_mailbox('archive') })
+            })
+
+          }
+          
+          
+        }
+
+
+
+
+        // If user clicks on the 'View' button...
+        viewButton.addEventListener('click', function () {
           //console.log('This element has been clicked!')
           fetch(`/emails/${email.id}`)
             .then(response => response.json())
@@ -87,23 +168,31 @@ function load_mailbox(mailbox) {
               single_email(sender, recipients, subject, timestamp, body);
 
               // Set 'read' property to true, after user clicked the email
-              if(email.read === false) {
+              if (email.read === false) {
                 fetch(`/emails/${email.id}`, {
                   method: 'PUT',
                   body: JSON.stringify({
-                      read: true
+                    read: true
                   })
                 })
               }
-              
-              
+
+              // Archive/Unarchive button
+              const button = document.createElement('button');
+              button.innerHTML = () => { if (email.archived === false) { "Archive" } else { "Unarchive" } }
+              button.addEventListener('click', function () {
+                console.log('This button has been clicked!')
+              });
+              document.querySelector('#container').append(button);
+
+
             });
         });
 
         document.querySelector('#emails-view').append(newEmail);
       });
 
-      
+
 
     });
 }
